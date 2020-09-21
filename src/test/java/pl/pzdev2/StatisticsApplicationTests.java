80f82e987a9993ffcab75ad8079c67879a6f4464
pzdev2 <pzdev2@MacBook-Air-dom.local>
2020-09-21 10:51:34 +0200

    init commit

---
+++ src/test/java/pl/pzdev2/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pzdev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
