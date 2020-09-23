404fea0f9163559c43589e11a9f2e3f3d9923ed4
pzdev2 <pzdev2@gmail.com>
2020-09-23 13:35:04 +0200

    add template with all VirtuaLogs entities

---
+++ src/main/java/pl/pzdev2/virtua/VirtuaLogsControler.java
@@ -0,0 +1,25 @@
+package pl.pzdev2.virtua;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VirtuaLogsControler {
+	
+	VirtuaLogsRepository virtuaLogsRepository;
+
+	public VirtuaLogsControler(VirtuaLogsRepository virtuaLogsRepository) {
+		this.virtuaLogsRepository = virtuaLogsRepository;
+	}
+	
+	@GetMapping("/virtuaLogs")
+	String getVirtuaLogs(Model model) {
+		
+		var virtuaLogs = virtuaLogsRepository.findAll();
+		model.addAttribute("virtuaLogs", virtuaLogs);
+		
+		return "virtuaLogs";
+	}
+
+}
