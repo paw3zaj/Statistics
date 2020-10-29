66d1c46bba9e904ea1943f73432aebe2a504a435
pzdev2 <pzdev2@gmail.com>
2020-10-29 11:27:26 +0100

    adding a log with the number of scans send to server

--- src/main/java/pl/pzdev2/scanner/ScannerService.java
+++ src/main/java/pl/pzdev2/scanner/ScannerService.java
@@ -3,9 +3,13 @@ package pl.pzdev2.scanner;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pl.pzdev2.scanner.interfaces.ScannerHandler;
+import pl.pzdev2.utility.FormatDateTime;
 import pl.pzdev2.virtua.Virtua;
+import pl.pzdev2.virtua.VirtuaUpdateService;
 import pl.pzdev2.virtua.interfaces.VirtuaRepository;
 
 import java.util.ArrayList;
@@ -17,6 +21,9 @@ public class ScannerService implements ScannerHandler {
 
     private VirtuaRepository virtuaRepository;
     private List<ScannerData> badScans = new ArrayList<>();
+
+    private static final Logger LOG = LoggerFactory.getLogger(ScannerService.class);
+
 
     public ScannerService(VirtuaRepository virtuaRepository) {
         this.virtuaRepository = virtuaRepository;
@@ -49,7 +56,9 @@ public class ScannerService implements ScannerHandler {
     @Override
     public List<ScannerData> deserializationScans(String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-
-        return objectMapper.readValue(json, new TypeReference<>() {});
+        List<ScannerData> scansList = objectMapper.readValue(json, new TypeReference<>() {});
+        LOG.info("Liczba zeskanowanych książek: {}  przesłana na serwer: {}"
+                , scansList.size(), FormatDateTime.getDateTime());
+        return scansList;
     }
 }
