ea17048ecd3f2de541631d8f40f407c3d30938c2
pzdev2 <pzdev2@gmail.com>
2020-11-19 09:42:25 +0100

    add a method that counts bad scans

--- src/main/java/pl/pzdev2/scan/interfaces/BadScanRepository.java
+++ src/main/java/pl/pzdev2/scan/interfaces/BadScanRepository.java
@@ -1,7 +1,14 @@
 package pl.pzdev2.scan.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import pl.pzdev2.scan.BadScan;
 
 public interface BadScanRepository extends JpaRepository<BadScan, Long> {
+	
+	@Query(value = "select count(id) from scanner_logs s"
+			+ " where s.scan_type = 'bad' and"
+			+ " year(s.created_date) = ?1 and month(s.created_date) = ?2", nativeQuery = true)
+	int countIncorrectScansInMonth(int year, int month);
 }
