36a846021c8d710809d0d182a01b945016515b17
pzdev2 <pzdev2@gmail.com>
2020-11-26 10:21:00 +0100

    remove statistic feature from scan package

--- src/main/java/pl/pzdev2/scan/ScanService.java
+++ src/main/java/pl/pzdev2/scan/ScanService.java
@@ -4,19 +4,16 @@ import org.springframework.stereotype.Service;
 import pl.pzdev2.scan.interfaces.BadScanRepository;
 import pl.pzdev2.scan.interfaces.CorrectScanRepository;
 import pl.pzdev2.scan.interfaces.ScanHandler;
-import pl.pzdev2.scan.interfaces.ScanRepository;
 
 import java.util.List;
 
 @Service
 public class ScanService implements ScanHandler {
 
-    private ScanRepository scanRepository;
     private BadScanRepository badScanRepository;
     private CorrectScanRepository correctScanRepository;
 
-    public ScanService(ScanRepository scanRepository, BadScanRepository badScanRepository, CorrectScanRepository correctScanRepository) {
-        this.scanRepository = scanRepository;
+    public ScanService(BadScanRepository badScanRepository, CorrectScanRepository correctScanRepository) {
         this.badScanRepository = badScanRepository;
         this.correctScanRepository = correctScanRepository;
     }
@@ -29,16 +26,6 @@ public class ScanService implements ScanHandler {
     @Override
     public void saveCorrectScan(List<CorrectScan> correctScans) {
         correctScanRepository.saveAll(correctScans);
-    }
-
-    @Override
-    public int countBadScans(int year, int month) {
-        return badScanRepository.countIncorrectScansInMonth(year, month);
-    }
-
-    @Override
-    public int countAllScans(int year, int month) {
-        return scanRepository.countAllScansInMonth(year, month);
     }
 
 }
--- src/main/java/pl/pzdev2/scan/interfaces/ScanHandler.java
+++ src/main/java/pl/pzdev2/scan/interfaces/ScanHandler.java
@@ -2,7 +2,6 @@ package pl.pzdev2.scan.interfaces;
 
 import pl.pzdev2.scan.BadScan;
 import pl.pzdev2.scan.CorrectScan;
-import pl.pzdev2.scanner.ScannerData;
 
 import java.util.List;
 
@@ -10,6 +9,4 @@ public interface ScanHandler {
 
     void saveBadScans(List<BadScan> badScans);
     void saveCorrectScan(List<CorrectScan> correctScans);
-    int countBadScans(int year, int month);
-    int countAllScans(int year, int month);
 }
