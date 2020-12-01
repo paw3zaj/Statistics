5a3f6b3c736515b6934b962c92326988007ca999
pzdev2 <pzdev2@gmail.com>
2020-12-01 13:19:09 +0100

    move the table structure to an external file

--- frontend/src/History.js
+++ frontend/src/History.js
@@ -1,49 +1,6 @@
 import React from "react"
-import './history.css'
-import { useTable } from 'react-table'
-
-function Table({ columns, data }) {
-    // Use the state and functions returned from useTable to build your UI
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-    } = useTable({
-      columns,
-      data,
-    })
+import Table from "./Table"
   
-    // Render the UI for your table
-    return (
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    )
-  }
-  
-
 const History = ({data}) => {
 
     const columns = React.useMemo(
