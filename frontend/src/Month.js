1ce6d36a02b30a90e75dacc1d77ff6a2d953c5dc
pzdev2 <pzdev2@gmail.com>
2021-02-01 12:44:43 +0100

    select statistics: monthly or yearly

--- frontend/src/Month.js
+++ frontend/src/Month.js
@@ -1,110 +1,123 @@
-import React, { useState } from "react"
+import React from "react"
 import Table from './Table'
 import axios from 'axios'
-import { SERVER_URL } from './constant'
-
+import {SERVER_URL} from './constant'
 
 const Month = ({
-    data,
-    setData,
-    year,
-    setYear,
-    month,
-    setMonth,
-    
-  }) => {
-     
-      // const [data, setData] = useState([])
-      // const [year, setYear] = useState('2019')
-      // const [month, setMonth] = useState('3')
-  
-      const url = SERVER_URL;
+                   data,
+                   setData,
+                   year,
+                   setYear,
+                   month,
+                   setMonth,
+                   all,
+                   setAll,
+                   error,
+                   setError
+               }) => {
 
-      const allScans = "34"
-      const errors = "3"
-  
-      const handleDownload = (e) => {
-        (async function fetchData() {
-          const response = await axios.get(url + '/scannerLogsBy?year='+year+'&month='+month);
-          setData(response.data);
-        })();
-      };
-  
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Index',
-        accessor: 'createdDate',
-      },
-      {
-        Header: 'Sygnatura',
-        accessor: 'virtua.signature'
-      },
-      {
-        Header: 'Kod kreskowy',
-        accessor: 'virtua.barcode'
-      },
-      {
-        Header: 'Autor',
-        accessor: 'virtua.author',
-      },
-      {
-        Header: 'Tytół',
-        accessor: 'virtua.title'
-      },
-      {
-        Header: 'Ilość wyporzyczeń',
-        accessor: 'barcode'
-      }
-    ],
-    []
-  )
+    const url = SERVER_URL;
 
-  return (
-    <div>
-     
-      {/* <form onSubmit={handleSubmit}> */}
-          <label>
-            Wybierz rok\miesiąc:
+    async function fetchDataInMonth() {
+        await axios.get(`${url}/countAllScans?year=${year}&month=${month}`)
+            .then(response => {setAll(response.data)});
+        await axios.get(`${url}/countBadScans?year=${year}&month=${month}`)
+            .then(response => {setError(response.data)});
+        await axios.get(`${url}/getAllInMonth?year=${year}&month=${month}`)
+            .then(response => {setData(response.data)});
+    }
+
+    async function fetchDataInYear() {
+        await axios.get(`${url}/countAllScans?year=${year}&month=1`)
+            .then(response => {setAll(response.data)});
+        await axios.get(`${url}/countBadScans?year=${year}&month=1`)
+            .then(response => {setError(response.data)});
+        await axios.get(`${url}/getAllInYear?year=${year}`)
+            .then(response => {setData(response.data)});
+    }
+
+    const handleDownload = (e) => {
+        if(month === '0') {
+             return fetchDataInYear()
+        } else {
+             return fetchDataInMonth()
+        }
+    }
+
+    const columns = React.useMemo(
+        () => [
+            // {
+            //   Header: 'Index',
+            //   accessor: 'createdDate',
+            // },
+            {
+                Header: 'Sygnatura',
+                accessor: 'signature'
+            },
+            {
+                Header: 'Kod kreskowy',
+                accessor: 'barcode'
+            },
+            {
+                Header: 'Autor',
+                accessor: 'author',
+            },
+            {
+                Header: 'Tytół',
+                accessor: 'title'
+            },
+            {
+                Header: 'Ilość wyporzyczeń',
+                accessor: 'amount'
+            }
+        ],
+        []
+    )
+
+    return (
+        <div>
+
+            {/* <form onSubmit={handleSubmit}> */}
+            <label>
+                Wybierz miesiąc\rok:
             </label>
-            <select value = {year} onChange={e => setYear(e.currentTarget.value)}>
-              <option value='2019' selected>2019</option>
-              <option value='2020'>2020</option>
+
+            <select value={month} onChange={e => setMonth(e.currentTarget.value)}>
+                <option value='0'>Cały</option>
+                <option value='1'>Styczeń</option>
+                <option value='2'>Luty</option>
+                <option value='3'>Marzec</option>
+                <option value='4'>Kwiecień</option>
+                <option value='5'>Maj</option>
+                <option value='6'>Czerwiec</option>
+                <option value='7'>Lipiec</option>
+                <option value='8'>Sierpień</option>
+                <option value='9'>Wrzesień</option>
+                <option value='10'>Październik</option>
+                <option value='11'>Listopad</option>
+                <option value='12'>Grudzień</option>
             </select>
-            <select value = {month} onChange={e => setMonth(e.currentTarget.value)}>
-              <option value='0'></option>
-              <option value='1'>Styczeń</option>
-              <option value='2'>Luty</option>
-              <option value='3'>Marzec</option>
-              <option value='4'>Kwiecień</option>
-              <option value='5'>Maj</option>
-              <option value='6'>Czerwiec</option>
-              <option value='7'>Lipiec</option>
-              <option value='8'>Sierpień</option>
-              <option value='9'>Wrzesień</option>
-              <option value='10'>Październik</option>
-              <option value='11'>Listopad</option>
-              <option value='12'>Grudzień</option>
+            <select value={year} onChange={e => setYear(e.currentTarget.value)}>
+                <option value='2019'>2019</option>
+                <option value='2020'>2020</option>
             </select>
-          {/* </label> */}
-          <button  onClick={handleDownload}>pobierz</button>
-          
-          {/* <input type="submit" value="Wyślij" /> */}
-        {/* </form> */}
-      
-      <br />
-  <label>Całkowita ilość skanów: {allScans}, w tym błędnych skanów: {errors}</label>
-      {/* <Styles> */}
-      <Table
-        columns={columns}
-        data={data}
-        // setData={setData}
-        
-      />
-      {/* </Styles> */}
-    </div>
-  
-  );
-  }
-  
-  export default Month
+            {/* </label> */}
+            <button onClick={handleDownload}>pobierz</button>
+
+            {/* <input type="submit" value="Wyślij" /> */}
+            {/* </form> */}
+
+            <br/>
+            <label>Całkowita ilość skanów: {all}, w tym błędnych skanów: {error}</label>
+            {/* <Styles> */}
+            <Table
+                columns={columns}
+                data={data}
+            />
+            {/* </Styles> */}
+        </div>
+
+    );
+}
+
+export default Month
