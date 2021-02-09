00f8367781765bba3620c0f8b14d0c69f596eeb9
pzdev2 <pzdev2@gmail.com>
2021-02-09 12:50:04 +0100

    create interface methods with sql queries

--- src/main/java/pl/pzdev2/scan/interfaces/ScanRepository.java
+++ src/main/java/pl/pzdev2/scan/interfaces/ScanRepository.java
@@ -9,35 +9,46 @@ import java.util.List;
 
 public interface ScanRepository extends JpaRepository<Scan, Long> {
 
-	@Query(value = "select count(id) from scan s where s.scan_type = 'bad'"
-			+ " and year(s.created_date) = ?1 and month(s.created_date) = ?2"
+	@Query(value = "select count(id)" +
+			" from scan s" +
+			" where s.year = ? and s.month = ?", nativeQuery = true)
+	int countAllScansInMonth(int year, int month);
+
+	@Query(value = "select count(id)" +
+			" from scan s" +
+			" where s.scan_type = 'bad' and s.year = ? and s.month = ?"
 			, nativeQuery = true)
 	int countIncorrectScansInMonth(int year, int month);
 
-	@Query(value = "select count(id) from scan s"
-			+ " where s.year = ?1 and s.month = ?2", nativeQuery = true)
-	int countAllScansInMonth(int year, int month);
+	@Query(value = "select count(id)" +
+			" from scan s" +
+			" where s.year = ?", nativeQuery = true)
+	int countAllScansInYear(int year);
 
-
-	@Query(value = "select * from scan s where s.year = ?1"
-			+ " and s.month = ?2", nativeQuery = true)
-	List<Scan> findAllCorrectScansInMonth(int year, int month);
-
-	@Query(value = "select * from scan s where"
-			+ " s.year = ?1", nativeQuery = true)
-	List<Scan> findAllCorrectScansInYear(int year);
+	@Query(value = "select count(id)" +
+			" from scan s" +
+			" where s.scan_type = 'bad' and s.year = ?", nativeQuery = true)
+	int countIncorrectScansInYear(int year);
 
 	@Query(value = "select count(id) from scan s " +
 			"join virtua v on s.virtua_id_virtua = v.id_virtua " +
 			"where  s.year= ?1 and s.month = ?2 " +
 			"and v.barcode = ?3", nativeQuery = true)
-	int countMonthlyBookBorrowing(int year, int month, String barcode);
+	int countBookScansInMonth(int year, int month, String barcode);
 
-	@Query(value = "select ROW_NUMBER() over (order by count(s.id) desc) as id, v.author, v.title, count(s.id) as amount" +
+	@Query(value = "select ROW_NUMBER() over (order by count(s.id) desc) as id," +
+			" v.author, v.title, count(s.id) as amount" +
 			" from scan s join virtua v on s.virtua_id_virtua = v.id_virtua" +
 			" where s.year = ? and s.month = ?" +
 			" group by v.author, v.title" +
-			" order by amount desc"
-			, nativeQuery = true)
+			" order by amount desc", nativeQuery = true)
 	List<Month> countTotalScansInMonth(int year, int month);
+
+	@Query(value = "select ROW_NUMBER() over (order by count(s.id) desc) as id," +
+			" v.author, v.title, count(s.id) as amount" +
+			" from scan s join virtua v on s.virtua_id_virtua = v.id_virtua" +
+			" where s.year = ?" +
+			" group by v.author, v.title" +
+			" order by amount desc", nativeQuery = true)
+	List<Month> countTotalScansInYear(int year);
 }
