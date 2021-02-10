9e42ca04cad0d7ed80694dcd6f04b95249f0e80d
pzdev2 <pzdev2@gmail.com>
2021-02-10 17:49:03 +0100

    add no args constructor to scanner data class

--- src/main/java/pl/pzdev2/scanner/ScannerData.java
+++ src/main/java/pl/pzdev2/scanner/ScannerData.java
@@ -2,8 +2,10 @@ package pl.pzdev2.scanner;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ScannerData {
 
